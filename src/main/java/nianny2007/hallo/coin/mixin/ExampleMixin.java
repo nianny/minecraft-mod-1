453b7e416a32cd272e554a59e1599a870cae18f4
nianny <xiaonianhe@hotmail.com>
2021-11-16 13:00:15 +0800

    welp

--- src/main/java/nianny2007/hallo/coin/mixin/ExampleMixin.java
+++ src/main/java/nianny2007/hallo/coin/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package nianny2007.hallo.coin.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import nianny2007.hallo.coin.HalloCoin;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		HalloCoin.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
